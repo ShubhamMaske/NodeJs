15b0f71277786bebc0927b1ea5b5559aa756ad07
ShubhamMaske <shubhammaske099@gmail.com>
2023-02-20 12:33:28 +0530

    first commit in Express example

---
+++ app1.js
@@ -0,0 +1,15 @@
+
+const express = require('express');
+
+const app = express();
+app.use((req, res, next) => {
+    console.log("The first middleware");
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log("The another middleware");
+    res.send('<h1> Hello form Express</h1>');
+});
+
+app.listen(3000);
