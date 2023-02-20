af0ac4cce2d08af746d1ede7b39d9a865567eb89
ShubhamMaske <shubhammaske099@gmail.com>
2023-02-20 13:27:12 +0530

    Third Commit

--- app1.js
+++ app1.js
@@ -1,15 +1,29 @@
 
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const app = express();
-app.use((req, res, next) => {
-    console.log("The first middleware");
-    console.log("Hello");
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/',(req, res, next) => {
+    console.log("The always runs");
     next();
 });
 
-app.use((req, res, next) => {
-    console.log("The another middleware");
+app.use('/add-product',(req, res, next) => {
+    console.log("The first middleware");
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size" placeholder="enter size"><button type="submit">Add Product</button></form>')
+});
+
+app.use('/product',(req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/',(req, res, next) => {
+    
     res.send('<h1> Hello form Express</h1>');
 });
 
