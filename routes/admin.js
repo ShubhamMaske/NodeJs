acb7f825d9f844f3bb0b5986d4e124517eff1359
ShubhamMaske <shubhammaske099@gmail.com>
2023-02-26 13:08:06 +0530

    first commit in dynamic route

--- routes/admin.js
+++ routes/admin.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 
-
+const detailController = require('../controller/details');
 router.get('/add-product',(req, res, next) => {
     
     res.sendFile(path.join(rootDir,'views','add-product.html'));
@@ -15,5 +15,7 @@ router.post('/add-products',(req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
+
+router.get('/get-product/:productId',detailController.detailpage);
 
 module.exports = router;
