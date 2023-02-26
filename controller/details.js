5df117c2e6204637e60d2abe8043a2ed64ab5b3a
ShubhamMaske <shubhammaske099@gmail.com>
2023-02-26 13:11:27 +0530

    first commit in dynamic route

--- controller/details.js
+++ controller/details.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const rootDir = require('../helper/path');
-
+//
 exports.detailpage = (req, res, next) => {   
     res.send('Product with product Id:'+req.params.productId);
 }
